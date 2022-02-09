16bcd76ba2e83132a41fe2590387535ac0d63a33
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2022-02-08 22:57:08 -0500

    Added email form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -20,40 +20,7 @@ export class ContactComponent implements OnInit {
 
   onSubmit(): void{
     
-    alert("Contact Test");
-    
   }
-
-  // public ValidateEmail():void{
-	// let mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
-  // let email = document.getElementById("email").innerHTML;
-	// if(email.match(mailformat))
-	// {
-	// 	alert("This is not a valid email address");
-	// 	}
-
-  // else()=>{
-  //     document.getElementById('emailError').innerHTML = "Invalid email format. Please try again.";
-  // }
-    
-  // }
-
-
-  // public sendEmail():void{
-    
-  //   const info = await nodemailer.createTransport.sendMail({
-  //     from: '"Sender Name" <from@example.net>',
-  //     to: "to@example.com",
-  //     subject: "Hello from node",
-  //     text: "Hello world?",
-  //     html: "<strong>Hello world?</strong>",
-  //     headers: { 'x-myheader': 'test header' }
-  //   });
-  
-  //   console.log("Message sent: %s", info.response);
-  // }
-  // }
   
 
 }
