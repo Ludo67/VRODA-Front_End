37dfcc420b328cdba4f274e481995db7cfdcc268
ludovic marcoux hoyos <ludomarcoux098@gmail.com>
2022-02-24 08:55:14 -0500

    Fixed Urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api/gateway'
+  apiBaseUrl: 'https://vrodapp.herokuapp.com/', //Product
+  apiBaseUrl2: 'https://vrodapp2.herokuapp.com/', //Auth
+  apiBaseUrl3: 'https://vrodapp3.herokuapp.com/' //Cart
 };
